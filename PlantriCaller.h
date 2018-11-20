c6059e9920d14fbb449824e6185a87bdac4f9262
Daniel Haden <haden.daniel@gmail.com>
2018-11-20 16:15:17 -0700

    Initial commit

---
+++ PlantriCaller.h
@@ -0,0 +1,19 @@
+//
+// Created by hadend on 11/20/18.
+//
+
+#ifndef PITRI_PLANTRICALLER_H
+#define PITRI_PLANTRICALLER_H
+
+
+#include <string>
+
+class PlantriCaller {
+public:
+    PlantriCaller();
+
+    std::string get(std::string parameters);
+};
+
+
+#endif //PITRI_PLANTRICALLER_H
