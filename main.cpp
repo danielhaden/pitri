a40db79ade999de3da41330ee7265ce85a646035
Daniel Haden <haden.daniel@gmail.com>
2018-12-11 17:57:51 -0700

    linked Qt libraries and tested simple gui

--- main.cpp
+++ main.cpp
@@ -6,34 +6,44 @@
 #include "VertexTable.h"
 #include <list>
 
+#include <QApplication>
+#include <QPushButton>
+
 typedef std::pair<int, int> E;
 typedef std::map<int, std::list<int> > G;
 
 using std::cout;
 using std::endl;
 
-int main() {
-    Graph g;
-    cout << g.e() << endl;
-    g+E(2,4);
-    cout << g.e() << endl;
-    g+E(2,3);
-    cout << g.e() << endl;
-    g+E(3,4);
-    cout << g.e() << endl;
-    g+E(4,5);
-    cout << g.e() << endl;
-    g+E(4,5);
-    cout << g.e() << endl;
-    g-E(4,5);
-    cout << g.e() << endl;
-    g-E(1,8);
-    cout << g.e() << endl;
+int main(int argc, char **argv) {
+    QApplication app (argc, argv);
 
-    Graph k('K', 10);
+    QPushButton button;
+    button.setText("My text");
+    button.setToolTip("A tooltip");
+    button.show();
 
-
-    cout << k << endl;
+//    Graph g;
+//    cout << g.e() << endl;
+//    g+E(2,4);
+//    cout << g.e() << endl;
+//    g+E(2,3);
+//    cout << g.e() << endl;
+//    g+E(3,4);
+//    cout << g.e() << endl;
+//    g+E(4,5);
+//    cout << g.e() << endl;
+//    g+E(4,5);
+//    cout << g.e() << endl;
+//    g-E(4,5);
+//    cout << g.e() << endl;
+//    g-E(1,8);
+//    cout << g.e() << endl;
+//
+//    Graph k('K', 10);
+//
+//
+//    cout << k << endl;
 
     // implement string constructor
     // overload + and *
@@ -45,5 +55,5 @@ int main() {
 
 
 
-    return 0;
+    return app.exec();
 }
